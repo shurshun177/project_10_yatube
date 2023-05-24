e206aa2e5d685f5edc477bd52134ba94327264e7
shurshun177 <artur.k177@gmail.com>
2023-05-24 14:06:12 +0300

    new commit

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -44,9 +44,7 @@ class APIPost(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class APIPostDetail(APIView):
-    # new class
-    pass
+
 
 
 #generics
